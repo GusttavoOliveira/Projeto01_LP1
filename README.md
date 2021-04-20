e4976cf5baa5b46971d199eef0306c65b6f6d580
Joaquim Breno <62159887+JoaquimBreno@users.noreply.github.com>
2021-04-20 20:49:19 -0300

    Correções finais

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projeto01_LP1
-Esse projeto busca criar um sistema para o gerenciamento de insumos para Covid-19
--- cadastraInsumos.h
+++
@@ -1,7 +0,0 @@
-#ifndef CADASTRAINSUMOS_H_INCLUDED
-#define CADASTRAINSUMOS_H_INCLUDED
-
-int cadastraInsumos();
-
-
-#endif // CADASTRAINSUMOS_H_INCLUDED
